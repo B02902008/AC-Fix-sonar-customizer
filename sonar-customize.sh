c1092e81fb7d3e8d3957fa0d05c31e2894f371e5
SE2019Group1 <r07922037@ntu.edu.tw>
2020-07-31 14:01:31 +0800

    Hide homepage link when not found

--- sonar-customize.sh
+++ sonar-customize.sh
@@ -1,3 +1,3 @@
 sonarHome='/opt/sonarqube'
 sed -i -e 's/SecurityServletFilter/CustomSecurityServletFilter/g' $sonarHome/web/WEB-INF/web.xml
-sed -i -e 's/<style>/<style>#footer,#global-navigation{display:none}body::-webkit-scrollbar{display:none}body{-ms-overflow-style:none;scrollbar-width:none}/g' $sonarHome/web/index.html
+sed -i -e 's/<style>/<style>#nonav>p>a{display:none}#footer,#global-navigation{display:none}body::-webkit-scrollbar{display:none}body{-ms-overflow-style:none;scrollbar-width:none}/g' $sonarHome/web/index.html
